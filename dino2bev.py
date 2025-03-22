e5a8e4632082241db7331d91b0a3fe5d77520fd6
Stefan J. Betz <info@stefan-betz.net>
2025-03-22 10:12:49 +0100

    improve the readability of our calculation, so people could control it

--- dino2bev.py
+++ dino2bev.py
@@ -69,25 +69,56 @@ argparser.add_argument(
 )
 
 
+def calculate(
+    price_per_liter: float,
+    l_per_100km: float,
+    distance_per_year: float,
+    ice_tax_per_year: float,
+    bev_tax_per_year: float,
+    thg_per_year: float,
+    kwh_per_100km: float,
+) -> float:
+    """
+    This function does the math we need, it does it in a way that people could
+    easily cross check, so we use comments and explain what we are doing!
+    """
+    # Distance per year is a total number, we need it per 100km
+    distance_in_100km = distance_per_year / 100
+    # Let's start what costs our Diesel or Gasoline per year
+    ice_consumption_cost_per_year = price_per_liter * l_per_100km * distance_in_100km
+    # Now we add the taxes
+    ice_cost_per_year = ice_consumption_cost_per_year + ice_tax_per_year
+    # Now we add the THG quote, while it is an advantage for an BEV, we could
+    # simply add as disadvantage for an ICE. An alternate solution would be
+    # to break the THG quote down to our yearly distance, but the result will
+    # be the same.
+    ice_cost_per_year_with_thg = ice_cost_per_year + thg_per_year
+    # Now we need to worry about an (possible) tax of our BEV:
+    bev_cost_per_year = ice_cost_per_year_with_thg + bev_tax_per_year
+    bev_cost_per_100km = bev_cost_per_year / distance_in_100km
+    bev_cost_per_kwh = bev_cost_per_100km / kwh_per_100km
+    return bev_cost_per_kwh
+
+
 if __name__ == "__main__":
     args = argparser.parse_args()
-    cost_per_kwh_diesel = (
-        (
-            (args.diesel_price * args.diesel_per_100km * args.distance)
-            + args.diesel_tax
-            + args.thg_quote
-        )
-        / args.distance
-        / args.kwh_per_100km
+    cost_per_kwh_diesel = calculate(
+        price_per_liter=args.diesel_price,
+        l_per_100km=args.diesel_per_100km,
+        distance_per_year=args.distance,
+        ice_tax_per_year=args.diesel_tax,
+        bev_tax_per_year=args.bev_tax,
+        thg_per_year=args.thg_quote,
+        kwh_per_100km=args.kwh_per_100km,
     )
     print(cost_per_kwh_diesel)
-    cost_per_kwh_gasoline = (
-        (
-            (args.gasoline_price * args.gasoline_per_100km * args.distance)
-            + args.gasoline_tax
-            + args.thg_quote
-        )
-        / args.distance
-        / args.kwh_per_100km
+    cost_per_kwh_gasoline = calculate(
+        price_per_liter=args.gasoline_price,
+        l_per_100km=args.gasoline_per_100km,
+        distance_per_year=args.distance,
+        ice_tax_per_year=args.gasoline_tax,
+        bev_tax_per_year=args.bev_tax,
+        thg_per_year=args.thg_quote,
+        kwh_per_100km=args.kwh_per_100km,
     )
     print(cost_per_kwh_gasoline)
